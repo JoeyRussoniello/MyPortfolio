622d96f9904d69b7f03b36da765129221600d0b9
JOSEPH RUSSONIELLO <jrussoniello@trinity-pm.com>
2025-08-13 13:30:26 -0700

    Updated data JSONs and images to reflect new skills

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
 
-import styles from './Hero.module.css';
 import { getImageUrl } from '../../utils';
+import styles from './Hero.module.css';
 
 const pdf = getImageUrl('hero/resume.pdf');
 export const Hero = () =>{
     return (
         <section className={styles.container}>
             <div className={styles.content}>
-                <h1 className={styles.title}>Hi, I'm Joey</h1>
-                <p className={styles.description}>I'm a Data Scientist with 4 years of experience who loves transforming data into powerful insights and solutions. Reach out if you'd like to learn more!</p>
+                <h1 className={styles.title}>Hi, I'm Joey!</h1>
+                <p className={styles.description}>I'm a Data Scientist with 3 years of working experience. From Rust compilers to polished Business Intelligence Dashboards, I love making tools that are ready for the real world, transforming data into actionable insights and solutions.</p>
                 <div className={styles.buttons}>
                     <a href='mailto:jmrusso@bu.edu' className={styles.contactBtn}>Contact Me</a>
                     <a href={pdf} className={styles.pdfBtn}>Resume</a>
